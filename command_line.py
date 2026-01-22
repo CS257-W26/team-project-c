4c1631a1dc702a151e3ad0589b4b832136c7a949
Gabe-Rose <roseg@carleton.edu>
2026-01-22 14:52:40 -0600

    Update command_line.py

--- command_line.py
+++ command_line.py
@@ -1,4 +1,34 @@
-'''
-The eventual location for the command line interface (CLI) for the project.
-This will be the entry point for the project when run from the command line.
-'''
+"""*US is considered a State*""" 
+
+#Gabe
+def main():
+  """Get user input, check user input, Call correct functions, call displaying data"""
+  """map flags to list of flags for get data function (--prices, --emmissions)"""
+
+"""list of states and list of tags must only have valid entries"""
+"""flags will be a list that describes what filters we want to use, each is a bool"""
+"""[prices, emmissions]"""
+
+#Hongmiao
+def getData("list of states", "list of flags"):
+
+  """returns array of dicts"""
+
+#Hongmiao
+def getEmmissionsData("State"):
+
+#Rafael
+def getPriceData("State"):
+
+#Rafael
+def getUSData():
+
+#Gabe
+def displayData("an array of dictionaries"):
+
+#Rafael
+def showHelp(): 
+
+
+if __name__ == "__main__":
+    main()
