881884061843ad452a809dc010650619f82d415f
Gabe-Rose <roseg@carleton.edu>
2026-02-06 09:30:18 -0600

    finished createtable.sql

--- ProductionCode/createtable.sql
+++ ProductionCode/createtable.sql
@@ -11,5 +11,32 @@ CREATE TABLE emissions ( --Create the table--
   quantityOfFuelConsumed BIGINT,
   fuelUnits TEXT,
   tonsco2Emissions BIGINT,
-  metricTonnesco2Emissions BIGINT,
+  metricTonnesco2Emissions BIGINT
+);
+
+DROP TABLE IF EXISTS sales_revenue;
+CREATE TABLE sales_revenue ( --Create the table--
+  year INTEGER,
+  month INTEGER,
+  state TEXT,
+  residentialRevenue BIGINT,
+  residentialSales BIGINT,
+  residentialCustomers BIGINT,
+  residentialPrice BIGINT,
+  commercialRevenue BIGINT,
+  commercialSales BIGINT,
+  commercialCustomers BIGINT,
+  commercialPrice BIGINT,
+  industrialRevenue BIGINT,
+  industrialSales BIGINT,
+  industrialCustomers BIGINT,
+  industrialPrice BIGINT,
+  transportationRevenue BIGINT,
+  transportationSales BIGINT,
+  transportationCustomers BIGINT,
+  transportationPrice BIGINT,
+  totalRevenue BIGINT,
+  totalales BIGINT,
+  totalCustomers BIGINT,
+  totalPrice BIGINT
 );
