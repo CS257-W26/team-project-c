82b07f00534cae3b7537a36c5e9af39597178de9
Rafael <washburnr@carleton.edu>
2026-02-12 21:16:20 -0500

    Better Coverage?

--- Tests/test_data_source.py
+++ Tests/test_data_source.py
@@ -1,65 +1,62 @@
 '''Tests for data source'''
 import unittest
-from unittest.mock import patch
-
+from unittest.mock import MagicMock
 from ProductionCode.data_source import DataSource
 
 class DataSourceTests(unittest.TestCase):
     '''Class containing tests of Data Soure class'''
     def setUp(self):
-        '''create DataSource object to be used'''
+        '''create mock data source object to be used'''
+        self.patcher = patch("datasource.Database")
+        self.mock_database_class = self.patcher.start()
+
+        self.mock_db_instance = MagicMock()
+        self.mock_database_class.return_value = self.mock_db_instance
+
         self.test_source = DataSource()
 
-    @patch.object(DataSource, "get_sales_state_year")
     def test_sales_state_year(self, mock_get_sales_state_year):
-        """Tests queries for sales for given year and state"""
-        mock_get_sales_state_year.return_value = {
+        '''Tests queries for sales for given year and state'''
+        self.mock_db_instance.query.return_value.first.return_value.as_dict.return_value = {
             "commercialRevenue": 3059902.25,
             "totalPrice": 12.54
         }
 
-        test_al_25 = self.test_source.get_sales_state_year("AL", 2025)
+        result = self.test_source.get_sales_state_year("AL", 2025)
 
-        self.assertEqual(test_al_25["commercialRevenue"], 3059902.25)
-        self.assertEqual(float(test_al_25["totalPrice"]), 12.54)
+        self.assertEqual(result["commercialRevenue"], 3059902.25)
+        self.assertEqual(float(result["totalPrice"]), 12.54)
 
-        mock_get_sales_state_year.assert_called_once_with("AL", 2025)
 
-    @patch.object(DataSource, "get_emissions_state_year")
     def test_emissions_state_year(self, mock_get_emissions_state_year):
-        """Tests queries for emissions for given year and state"""
-        mock_get_emissions_state_year.return_value = {
+        '''Tests queries for emissions for given year and state'''
+        self.mock_db_instance.query.return_value.first.return_value.as_dict.return_value = {
             "generation": 27814120731.0,
             "thermalOutput": 17095959.0
         }
 
-        test_mn_24 = self.test_source.get_emissions_state_year("MN", 2024)
+        result = self.test_source.get_emissions_state_year("MN", 2024)
 
-        self.assertEqual(test_mn_24["generation"], 27814120731.0)
-        self.assertEqual(test_mn_24["thermalOutput"], 17095959.0)
+        self.assertEqual(result["generation"], 27814120731.0)
+        self.assertEqual(result["thermalOutput"], 17095959.0)
 
-        mock_get_emissions_state_year.assert_called_once_with("MN", 2024)
-
-    @patch.object(DataSource, "get_sales_us_year")
     def test_us_23_price(self, mock_get_sales_us_year):
-        """Test loading of US price"""
-        mock_get_sales_us_year.return_value = {
-            "commercialSales": 1408108756.0399995
+        '''Test loading of US price'''
+        self.mock_db_instance.query.return_value.first.return_value.as_dict.return_value = {
+            "commercialSales": 1408108756.04
         }
 
-        test_us_23 = self.test_source.get_sales_us_year(2023)
+        result = self.test_source.get_sales_us_year(2023)
 
-        self.assertEqual(test_us_23["commercialSales"], 1408108756.0399995)
-        mock_get_sales_us_year.assert_called_once_with(2023)
+        self.assertEqual(result["commercialSales"], 1408108756.04)
 
-    @patch.object(DataSource, "get_emissions_us_year")
     def test_us_23_emissions(self, mock_get_emissions_us_year):
-        """Test loading of US emissions"""
-        mock_get_emissions_us_year.return_value = {
+        '''Test loading of US emissions'''
+        self.mock_db_instance.query.return_value.first.return_value.as_dict.return_value = {
             "generation": 2526745161497
         }
 
-        test_us_23 = self.test_source.get_emissions_us_year(2023)
+        result = self.test_source.get_emissions_us_year(2023)
 
-        self.assertEqual(test_us_23["generation"], 2526745161497)
-        mock_get_emissions_us_year.assert_called_once_with(2023)
+        self.assertEqual(result["generation"], 2526745161497)
+        self.assertEqual(result["state"], "US")
