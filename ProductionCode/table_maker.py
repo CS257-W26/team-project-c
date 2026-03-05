8897c64eb0878bba8d55ba78e44d157d6485a5e2
Gabe-Rose <roseg@carleton.edu>
2026-03-05 13:39:14 -0600

    added table_maker docstrings

--- ProductionCode/table_maker.py
+++ ProductionCode/table_maker.py
@@ -6,9 +6,13 @@ from ProductionCode.data_processing import format_string
 from ProductionCode.config import DISPLAY_ALIASES
 
 class TableMaker:
+"""Class to construct text formatted tables for command line interface"""
 
-    """each entry is a dictionary"""
     def __init__(self, data=None):
+        """
+        contructor
+        param data: data to be displayed in the table (optional)
+        """
         if data is None:
             data = []
         self.entries = []
@@ -19,16 +23,23 @@ class TableMaker:
                 self.add_new_entry(entry)
 
     def add_new_empty_entry(self, state, year):
-        """Adds a new empty entry for the state and year. If one exists throws an error"""
+        """
+        Adds a new empty entry for the state and year. If one exists throws an error
+        param state: two letter state code of entry to create
+        param year: year of entry to create
+        """
         for entry in self.entries:
             if entry.get("state") == state and entry.get("year") == year:
                 raise KeyError("Entry already exists")
         self.entries.append({"state" : state, "year" : year})
 
     def add_new_entry(self, entry):
-        """add a new entry. Throws an error if one already exists for state, year
+        """
+        Add a new entry. Throws an error if one already exists for state, year
         entry is a dictionary, must contain "state" and "year".
-        Dictionary entries not listed in display Alliases will not be printed"""
+        Dictionary entries not listed in display Alliases will not be printed
+        param entry: dictionary containing data for a new entry. Must include state and year
+        """
         if not isinstance(entry, dict):
             raise TypeError("Entry must be of <class 'dict'>")
         for row in self.entries:
@@ -37,10 +48,14 @@ class TableMaker:
         self.entries.append(entry)
 
     def add_data_for_entry(self, state, year, data):
-        """adds a new data entry for the state and year.
+        """
+        Adds a new data entry for the state and year.
         raises an error if entry for state and year does not exist 
-        data is a tuple (key, data) and key must be an acceptable key for this class
-        (See DISPLAY_ALIASES)"""
+        param state: state to put data under
+        param year: year to put data under
+        param data: a tuple (key, data) and key must be an acceptable key for this class
+        (See DISPLAY_ALIASES)
+        """
         if not isinstance(data, tuple):
             raise TypeError("Data must be a tuple of (key, value)")
         for entry in self.entries:
@@ -50,9 +65,11 @@ class TableMaker:
         raise KeyError("Entry does not exists")
 
     def add_comparison_entry(self, entry):
-        """add a new comparison. Throws an error if jey state != 'comparison'
-        entry is a dictionary, must contain 'state': 'comparison'.
-        Dictionary entries not listed in display Alliases will not be printed"""
+        """
+        Add a new comparison. Throws an error if key state != 'comparison'
+        Dictionary entries not listed in display Alliases will not be printed
+        param entry: a dictionary of comparison data, must contain 'state': 'comparison'.
+        """
         if not isinstance(entry, dict):
             raise TypeError("Entry must be of <class 'dict'>")
         if entry.get('state') != 'comparison':
@@ -60,7 +77,9 @@ class TableMaker:
         self.entries.append(entry)
 
     def get_table(self):
-        """Displays the table"""   
+        """
+        Get the formated table
+        return table_str: table formated as a string"""   
         buffer = io.StringIO()
         col_sizes = self.get_col_sizes()
 
@@ -81,7 +100,12 @@ class TableMaker:
         return table_str
 
     def get_table_row(self, alias, col_sizes):
-        """returns a fromated row for the table"""
+        """
+        Returns a fromated row for the table
+        param alias: tuple containing key to search for and lable to display on this row
+        param col_sizes: array of ints specifying the width of each col
+        return line: a row of the table formated as a string.
+        """
         line = alias[1]
         for j, _ in enumerate(self.entries):
             if self.entries[j].get(alias[0]) is None:
@@ -94,14 +118,20 @@ class TableMaker:
         return line
 
     def is_row_empty(self, row_name):
-        """returns True if one or more entries is present in a row. Used for print_table()"""
+        """
+        Returns True if one or more entries is present in a row.
+        param row_name: key for row that we want to test
+        return: Boolean"""
         for entry in self.entries:
             if entry.get(row_name) is not None:
                 return False
         return True
 
     def get_col_sizes(self):
-        """gets the width for each col based on the widest piece of data (character wise)"""
+        """
+        Gets the width for each col based on the widest piece of data (character wise)
+        returns sizes: array of ints specifying each column's size
+        """
         sizes = []
         for entry in self.entries:
             largest_entry = 4
