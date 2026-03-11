0f09a75dcd9a77b0429a59815fd84887e4b3c6c6
Gabe-Rose <roseg@carleton.edu>
2026-03-10 19:11:59 -0500

    ...

--- ProductionCode/config.py
+++ ProductionCode/config.py
@@ -83,71 +83,134 @@ AVAILABLE_YEARS = [
     '2013'
 ]
 
+# (Dict alias, TableMaker alias, sql col alias, General Title)
 ALIASES = [
-    ("state"                          , "state"                                    
-        , "State                           "),
-    ("year"                           , "year"                                     
-        , "Year                            "),
-    ("generation"                     , 'SUM(generation) as "generation"'          
-        , "Generation                 (kWh)"),
-    ("thermalOutput"                  , 'SUM(usefullthermaloutput) as "thermalOutput"'
-        , "Useful Thermal Output    (MMBtu)"),
-    ("totalFuelConsumption"           , 'SUM(totalfuelconsumption) as "totalFuelConsumption"'
-        , "Total Fuel Consumption   (MMBtu)"),
-    ("totalFuelConsumptionGeneration" , 'SUM(fuelconsumptionelectricgeneration) as "totalFuelConsumptionGeneration"'
-        , "Total Fuel Consumption for      |\nElectric Generation      (MMBtu)"),
-    ("fuelConsumptionThermalOutput"   , 'SUM(fuelconsumptionusefulthermaloutput) as "fuelConsumptionThermalOutput"'
-        , "Fuel Consumption for Useful     |\nThermal Output           (MMBtu)"),
-    ("quantityOfFuelConsumed"         , 'SUM(quantityoffuelconsumed) as "quantityOfFuelConsumed"'
-        , "Quanitty of Fuel Units Consumed "),
-    ("co2Tons"                        , 'SUM(tonsco2emissions) as "co2Tons"'
-        , "CO2 Emmissions            (Tons)"),
-    ("co2MetricTons"                  , 'SUM(metrictonnesco2emissions) as "co2MetricTons"'
-        , "CO2 Emmissions     (Metric Tons)"),
-    ("residentialRevenue"             , 'Sum(residentialRevenue) as "residentialRevenue"'
-        , "Residential Revenue        ($1K)"),
-    ("residentialSales"               , 'Sum(residentialSales) as "residentialSales"'
-        , "Residential Sales          (MWh)"),
-    ("residentialCustomers"           , 'Round(Avg(residentialCustomers)::numeric, 2) as "residentialCustomers"'
-        , "Residential Customers           "),
-    ("residentialPrice"               , 'Round(Avg(residentialPrice)::numeric, 2) as "residentialPrice"'
-        , "Residential Price    (cents/kWh)"),
-    ("commercialRevenue"              , 'Sum(commercialRevenue) as "commercialRevenue"'
-        , "Commercial Revenue         ($1K)"),
-    ("commercialSales"                , 'Sum(commercialSales) as "commercialSales"'
-        , "Commercial Sales           (MWh)"),
-    ("commercialCustomers"            , 'Round(Avg(commercialCustomers)::numeric, 2) as "commercialCustomers"'
-        , "Commercial Customers            "),
-    ("commercialPrice"                , 'Round(Avg(commercialPrice)::numeric, 2) as "commercialPrice"'
-        , "Commercial Price     (cents/kWh)"),
-    ("industrialRevenue"              , 'Sum(industrialRevenue) as "industrialRevenue"'
-        , "Industrial Revenue         ($1K)"),
-    ("industrialSales"                , 'Sum(industrialSales) as "industrialSales"'
-        , "Industrial Sales           (MWh)"),
-    ("industrialCustomers"            , 'Round(Avg(industrialCustomers)::numeric, 2) as "industrialCustomers"'
-        , "Industrial Customers            "),
-    ("industrialPrice"                , 'Round(Avg(industrialPrice)::numeric, 2) as "industrialPrice"'
-        , "Industrial Price     (cents/kWh)"),
-    ("transportationRevenue"          , 'Sum(transportationRevenue) as "transportationRevenue"'
-        , "Transportation Revenue     ($1K)"),
-    ("transportationSales"            , 'Sum(transportationSales) as "transportationSales"'
-        , "Transportation Sales       (MWh)"),
-    ("transportationCustomers"        , 'Round(Avg(transportationCustomers)::numeric, 2) as "transportationCustomers"'
-        , "Transportation Customers        "),
-    ("transportationPrice"            , 'Round(Avg(transportationPrice)::numeric, 2) as "transportationPrice"'
+    ("state"                          
+        , "state"                                    
+        , "State                           "
+        , 'State'),
+    ("year"                           
+        , "year"                                     
+        , "Year                            "
+        , "Year"),
+    ("generation"                     
+        , 'SUM(generation) as "generation"'          
+        , "Generation                 (kWh)"
+        , "Generation (kWh)"),
+    ("thermalOutput"                  
+        , 'SUM(usefullthermaloutput) as "thermalOutput"'
+        , "Useful Thermal Output    (MMBtu)"
+        , "Useful Thermal Output (MMBtu)"),
+    ("totalFuelConsumption"           
+        , 'SUM(totalfuelconsumption) as "totalFuelConsumption"'
+        , "Total Fuel Consumption   (MMBtu)"
+        , "Total Fuel Consumption (MMBtu)"),
+    ("totalFuelConsumptionGeneration" 
+        , 'SUM(fuelconsumptionelectricgeneration) as "totalFuelConsumptionGeneration"'
+        , "Total Fuel Consumption for      |\nElectric Generation      (MMBtu)"
+        , "Total Fuel Consumption for Electric Generation (MMBtu)"),
+    ("fuelConsumptionThermalOutput"   
+        , 'SUM(fuelconsumptionusefulthermaloutput) as "fuelConsumptionThermalOutput"'
+        , "Fuel Consumption for Useful     |\nThermal Output           (MMBtu)"
+        , "Fuel Consumption for Useful Thermal Output (MMBtu)"),
+    ("quantityOfFuelConsumed"         
+        , 'SUM(quantityoffuelconsumed) as "quantityOfFuelConsumed"'
+        , "Quanitty of Fuel Units Consumed"
+        , "Quanitty of Fuel Units Consumed \
+        (but this is wrong and we need to get rid of it from UI)"),
+    ("co2Tons"                        
+        , 'SUM(tonsco2emissions) as "co2Tons"'
+        , "CO2 Emmissions            (Tons)"
+        , "CO2 Emmissions (Tons)"),
+    ("co2MetricTons"                  
+        , 'SUM(metrictonnesco2emissions) as "co2MetricTons"'
+        , "CO2 Emmissions     (Metric Tons)"
+        , "CO2 Emmissions (Metric Tons)"),
+    ("residentialRevenue"             
+        , 'Sum(residentialRevenue) as "residentialRevenue"'
+        , "Residential Revenue        ($1K)"
+        , "Residential Revenue ($1K)"),
+    ("residentialSales"               
+        , 'Sum(residentialSales) as "residentialSales"'
+        , "Residential Sales          (MWh)"
+        , "Residential Sales (MWh)"),
+    ("residentialCustomers"           
+        , 'Round(Avg(residentialCustomers)::numeric, 2) as "residentialCustomers"'
+        , "Residential Customers           "
+        , "Residential Customers"),
+    ("residentialPrice"               
+        , 'Round(Avg(residentialPrice)::numeric, 2) as "residentialPrice"'
+        , "Residential Price    (cents/kWh)"
+        , "Residential Price (cents/kWh)"),
+    ("commercialRevenue"              
+        , 'Sum(commercialRevenue) as "commercialRevenue"'
+        , "Commercial Revenue         ($1K)"
+        , "Commercial Revenue ($1K)"),
+    ("commercialSales"                
+        , 'Sum(commercialSales) as "commercialSales"'
+        , "Commercial Sales           (MWh)"
+        , "Commercial Sales (MWh)"),
+    ("commercialCustomers"            
+        , 'Round(Avg(commercialCustomers)::numeric, 2) as "commercialCustomers"'
+        , "Commercial Customers            "
+        , "Commercial Customers"),
+    ("commercialPrice"                
+        , 'Round(Avg(commercialPrice)::numeric, 2) as "commercialPrice"'
+        , "Commercial Price     (cents/kWh)"
+        , "Commercial Price (cents/kWh)"),
+    ("industrialRevenue"              
+        , 'Sum(industrialRevenue) as "industrialRevenue"'
+        , "Industrial Revenue         ($1K)"
+        , "Industrial Revenue ($1K)"),
+    ("industrialSales"                
+        , 'Sum(industrialSales) as "industrialSales"'
+        , "Industrial Sales           (MWh)"
+        , "Industrial Sales (MWh)"),
+    ("industrialCustomers"            
+        , 'Round(Avg(industrialCustomers)::numeric, 2) as "industrialCustomers"'
+        , "Industrial Customers            "
+        , "Industrial Customers"),
+    ("industrialPrice"                
+        , 'Round(Avg(industrialPrice)::numeric, 2) as "industrialPrice"'
+        , "Industrial Price     (cents/kWh)"
+        , "Industrial Price (cents/kWh)"),
+    ("transportationRevenue"          
+        , 'Sum(transportationRevenue) as "transportationRevenue"'
+        , "Transportation Revenue     ($1K)"
+        , "Transportation Revenue ($1K)"),
+    ("transportationSales"            
+        , 'Sum(transportationSales) as "transportationSales"'
+        , "Transportation Sales       (MWh)"
+        , "Transportation Sales (MWh)"),
+    ("transportationCustomers"        
+        , 'Round(Avg(transportationCustomers)::numeric, 2) as "transportationCustomers"'
+        , "Transportation Customers        "
+        , "Transportation Customers"),
+    ("transportationPrice"            
+        , 'Round(Avg(transportationPrice)::numeric, 2) as "transportationPrice"'
+        , "Transportation Price (cents/kWh)"
         , "Transportation Price (cents/kWh)"),
-    ("totalRevenue"                   , 'Sum(totalRevenue) as "totalRevenue"'
-        , "total Revenue              ($1K)"),
-    ("totalSales"                     , 'Sum(totalSales) as "totalSales"'
-        , "total Sales                (MWh)"),
-    ("totalCustomers"                 , 'Round(Avg(totalCustomers)::numeric, 2) as "totalCustomers"'
-        , "total Customers                 "),
-    ("totalPrice"                     , 'Round(Avg(totalPrice)::numeric, 2) as "totalPrice"'
-        , "total Price          (cents/kWh)")
+    ("totalRevenue"                   
+        , 'Sum(totalRevenue) as "totalRevenue"'
+        , "Total Revenue              ($1K)"
+        , "Total Revenue ($1K)"),
+    ("totalSales"                     
+        , 'Sum(totalSales) as "totalSales"'
+        , "Total Sales                (MWh)"
+        , "Total Sales (MWh)"),
+    ("totalCustomers"                 
+        , 'Round(Avg(totalCustomers)::numeric, 2) as "totalCustomers"'
+        , "Total Customers                 "
+        , "Total Customers"),
+    ("totalPrice"                     
+        , 'Round(Avg(totalPrice)::numeric, 2) as "totalPrice"'
+        , "Total Price          (cents/kWh)"
+        , "Total Price (cents/kWh)")
 ]
 
 SQL_ALIASES = [(x[0], x[1]) for x in ALIASES]
 DISPLAY_ALIASES = [(x[0], x[2]) for x in ALIASES]
+TITLE_ALIASES = [(x[0], x[3]) for x in ALIASES]
 
 DICTIONARY_KEYS_ORDERED = [x[0] for x in DISPLAY_ALIASES]
 DICTIONARY_KEYS_EMMISIONS_INDEXES = range(2,8)
--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,8 @@ from flask import Flask, request, render_template, url_for, redirect
 
 from ProductionCode import core
 from ProductionCode.config import AUTOCOMPLETE_OPTIONS, AUTOCOMPLETE_ALLIASES
-from ProductionCode.config import AVAILABLE_YEARS, DISPLAY_ALIASES
+from ProductionCode.config import  TITLE_ALIASES
+DATA_OPTIONS = [x for x in TITLE_ALIASES]
 from ProductionCode.plotting import PlotBuilder
 from flask_api import api
 
@@ -15,13 +16,13 @@ plotter = PlotBuilder()
 def homepage():
     """Route for hompage"""
     return render_template('index.html', autocomplete=AUTOCOMPLETE_OPTIONS, \
-        available_years=AVAILABLE_YEARS)
+        data_options=DATA_OPTIONS)
 
 @app.route('/map')
 def usmap():
     '''route for interactive map'''
     return render_template('map.html', autocomplete=AUTOCOMPLETE_OPTIONS, \
-        available_years=AVAILABLE_YEARS)
+        data_options=DATA_OPTIONS)
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -29,7 +30,7 @@ def search():
     state = request.form['state']
     state_index = AUTOCOMPLETE_OPTIONS.index(state)
     state_code = AUTOCOMPLETE_ALLIASES[state_index]
-    return redirect(url_for('bystate', state=state_code, year=request.form['year']))
+    return redirect(url_for('bystate', state=state_code, data_type=request.form['data_type']))
 
 @app.route("/bystate/<state>/<graph_type>/")
 def bystate(state, graph_type):
@@ -44,7 +45,7 @@ def bystate(state, graph_type):
         "bystate.html",
         autocomplete=AUTOCOMPLETE_OPTIONS,
         autocomplete_aliases=AUTOCOMPLETE_ALLIASES,
-        available_years=AVAILABLE_YEARS,
+        data_options=DATA_OPTIONS,
         state=state_data[0],
         plot_png=plot_base64,
     )
@@ -57,14 +58,14 @@ def compareutility():
         if state1 is not None:
             state1 = AUTOCOMPLETE_OPTIONS[AUTOCOMPLETE_ALLIASES.index(state1)]
         return render_template('compareutility.html', autocomplete=AUTOCOMPLETE_OPTIONS, \
-            available_years=AVAILABLE_YEARS, state1_autofill=state1)
+            data_options=DATA_OPTIONS, state1_autofill=state1)
     #POST
     state1 = request.form['state1']
     state2 = request.form['state2']
     state1_index = AUTOCOMPLETE_OPTIONS.index(state1)
     state2_index = AUTOCOMPLETE_OPTIONS.index(state2)
     agg_state_code = AUTOCOMPLETE_ALLIASES[state1_index] + AUTOCOMPLETE_ALLIASES[state2_index]
-    return redirect(url_for('compare_states', states=agg_state_code, year=request.form['year']))
+    return redirect(url_for('compare_states', states=agg_state_code, data_type=request.form['data_type']))
 
 @app.route('/compare/<states>/<graph_type>/')
 def compare_states(states, graph_type):
@@ -80,7 +81,7 @@ def compare_states(states, graph_type):
         "compare.html",
         autocomplete=AUTOCOMPLETE_OPTIONS,
         autocomplete_aliases=AUTOCOMPLETE_ALLIASES,
-        available_years=AVAILABLE_YEARS,
+        data_options=DATA_OPTIONS,
         state1=state_data[0][0],
         state2=state_data[1][0],
         plot_png=plot_base64,
@@ -91,13 +92,16 @@ def display_us_data():
     '''
     Endpoint for displaying US data
     '''
-    selected_year =  request.args.get("year", type=int)
+    graph_type =  request.args.get("graph_type", type=int)
     data = None
-    if selected_year:
-        data = core.get_us_year_data(selected_year)
-    return render_template('us.html',us_data = data,years = AVAILABLE_YEARS,
-    selected_year=selected_year, autocomplete=AUTOCOMPLETE_OPTIONS,
-    available_years=AVAILABLE_YEARS)
+    if graph_type:
+        data = core.get_graphable_data('US', graph_type)
+    plot = PlotBuilder()
+    plot.add_data(state_data)
+    plot_base64 = plot.get_fig()
+    return render_template('us.html',plot_png = plot_base64, data_options=DATA_OPTIONS,
+    selected_data=selected_data, autocomplete=AUTOCOMPLETE_OPTIONS,
+    data_options=DATA_OPTIONS)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -105,7 +109,7 @@ def page_not_found(e):
     return render_template('error.html', errorNumber=404, errorText="""
     Uh oh! The page you are looking for does not exist. Try going back to the homepage. 
     <br>""" + str(e),
-    autocomplete=AUTOCOMPLETE_OPTIONS, available_years=AVAILABLE_YEARS), 404
+    autocomplete=AUTOCOMPLETE_OPTIONS, data_options=DATA_OPTIONS), 404
 
 @app.errorhandler(500)
 def server_error(e):
@@ -113,7 +117,7 @@ def server_error(e):
     return render_template('error.html', errorNumber=500, errorText="""
     Uh oh! There was an internal server error. Sorry for the inconveniance.
     Try visiting our homepage.<br>""" + str(e),
-    autocomplete=AUTOCOMPLETE_OPTIONS, available_years=AVAILABLE_YEARS), 500
+    autocomplete=AUTOCOMPLETE_OPTIONS, data_options=DATA_OPTIONS), 500
 
 if __name__ == '__main__':
     app.register_blueprint(api, url_prefix='/api')
