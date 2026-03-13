c7c55442bc986279ce95e010708e5af2d1ea426c
Gabe-Rose <roseg@carleton.edu>
2026-03-13 12:24:13 -0500

    ...

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, url_for, redirect
 
 from ProductionCode import core
 from ProductionCode.config import AUTOCOMPLETE_OPTIONS, AUTOCOMPLETE_ALLIASES
-from ProductionCode.config import  TITLE_ALIASES
+from ProductionCode.config import TITLE_ALIASES, AVAILABLE_YEARS
 DATA_OPTIONS = [x[1] for x in TITLE_ALIASES]
 from ProductionCode.plotting import PlotBuilder
 from flask_api import api
@@ -15,14 +15,12 @@ plotter = PlotBuilder()
 @app.route('/')
 def homepage():
     """Route for hompage"""
-    return render_template('index.html', autocomplete=AUTOCOMPLETE_OPTIONS, \
-        data_options=DATA_OPTIONS)
+    return render_template('index.html', autocomplete=AUTOCOMPLETE_OPTIONS)
 
 @app.route('/map')
 def usmap():
     '''route for interactive map'''
-    return render_template('map.html', autocomplete=AUTOCOMPLETE_OPTIONS, \
-        data_options=DATA_OPTIONS)
+    return render_template('map.html', autocomplete=AUTOCOMPLETE_OPTIONS)
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -30,24 +28,39 @@ def search():
     state = request.form['state']
     state_index = AUTOCOMPLETE_OPTIONS.index(state)
     state_code = AUTOCOMPLETE_ALLIASES[state_index]
-    return redirect(url_for('bystate', state=state_code, graph_type=request.form['graph_type']))
+    return redirect(url_for('bystate', state=state_code))
 
-@app.route("/bystate/<state>/<graph_type>/")
-def bystate(state, graph_type):
+@app.route("/bystate/<state>/")
+def bystate(state):
     """Route for individual state data page."""
 
-    state_data = core.get_graph_data(state, graph_type)
-    plot = PlotBuilder()
-    plot.add_data(state_data)
-    plot_base64 = plot.get_fig()
+    year = request.args.get("year", type=int)
+    table_data = None
+    if year:
+        table_data = core.get_state_year_data(state, year)
+
+    graph_title = request.args.get("graph_title", type=str)
+    plot_base64 = None
+    if graph_title:
+        index = DATA_OPTIONS.index(state)
+        graph_title_alias = TITLE_ALIASES[index][0]
+        graph_data = core.get_graph_data(state, graph_title_alias)
+        plot = PlotBuilder()
+        plot.add_data(graph_data)
+        plot_base64 = plot.get_fig()
 
     return render_template(
         "bystate.html",
         autocomplete=AUTOCOMPLETE_OPTIONS,
-        autocomplete_aliases=AUTOCOMPLETE_ALLIASES,
         data_options=DATA_OPTIONS,
-        state=state_data[0],
+        year_options=AVAILABLE_YEARS,
+
+        state=state,
+        selected_graph=graph_title,
+        selected_year=year,
+
         plot_png=plot_base64,
+        table_data=table_data,
     )
 
 @app.route('/compareutility', methods=['GET', 'POST'])
@@ -58,33 +71,48 @@ def compareutility():
         if state1 is not None:
             state1 = AUTOCOMPLETE_OPTIONS[AUTOCOMPLETE_ALLIASES.index(state1)]
         return render_template('compareutility.html', autocomplete=AUTOCOMPLETE_OPTIONS, \
-            data_options=DATA_OPTIONS, state1_autofill=state1)
+            state1_autofill=state1)
     #POST
     state1 = request.form['state1']
     state2 = request.form['state2']
     state1_index = AUTOCOMPLETE_OPTIONS.index(state1)
     state2_index = AUTOCOMPLETE_OPTIONS.index(state2)
     agg_state_code = AUTOCOMPLETE_ALLIASES[state1_index] + AUTOCOMPLETE_ALLIASES[state2_index]
-    return redirect(url_for('compare_states', states=agg_state_code, graph_type=request.form['graph_type']))
+    return redirect(url_for('compare_states', states=agg_state_code))
 
-@app.route('/compare/<states>/<graph_type>/')
-def compare_states(states, graph_type):
+@app.route('/compare/<states>/')
+def compare_states(states):
     """route for comparison page"""
 
-    state_data = core.get_graph_data_comparison(states, graph_type)
-    plot = PlotBuilder()
-    plot.add_data(state_data[0])
-    plot.add_data(state_data[1])
-    plot_base64 = plot.get_fig()
+    year = request.args.get("year", type=int)
+    table_data = None
+    if year:
+        table_data = core.get_comparison_year(states, year)
+
+    graph_title = request.args.get("graph_title", type=str)
+    plot_base64 = None
+    if graph_title:
+        index = DATA_OPTIONS.index(state)
+        graph_title_alias = TITLE_ALIASES[index][0]
+        state_data = core.get_graph_data_comparison(states, graph_title_alias)
+        plot = PlotBuilder()
+        plot.add_data(state_data[0])
+        plot.add_data(state_data[1])
+        plot_base64 = plot.get_fig()
 
     return render_template(
         "compare.html",
         autocomplete=AUTOCOMPLETE_OPTIONS,
-        autocomplete_aliases=AUTOCOMPLETE_ALLIASES,
         data_options=DATA_OPTIONS,
-        state1=state_data[0][0],
-        state2=state_data[1][0],
+        year_options=AVAILABLE_YEARS,
+
+        state1=states[:2],
+        state2=states[2:],
+        selected_graph=graph_title,
+        selected_year=year,
+
         plot_png=plot_base64,
+        table_data=table_data,
     )
 
 @app.route('/us')
@@ -92,16 +120,31 @@ def display_us_data():
     '''
     Endpoint for displaying US data
     '''
-    graph_type =  request.args.get("graph_type", type=int)
-    data = None
+    graph_title = request.args.get("graph_title", type=str)
     plot_base64 = None
-    if graph_type:
-        data = core.get_graphable_data('US', graph_type)
+    if graph_title:
+        index = DATA_OPTIONS.index(state)
+        graph_title_alias = TITLE_ALIASES[index][0]
+        data = core.get_graphable_data('US', graph_title_alias)
         plot = PlotBuilder()
         plot.add_data(data)
         plot_base64 = plot.get_fig()
-    return render_template('us.html',plot_png = plot_base64, data_options=DATA_OPTIONS,
-    selected_data=graph_type, autocomplete=AUTOCOMPLETE_OPTIONS,)
+    
+    year = request.args.get("year", type=int)
+    table_data = None
+    if year:
+        table_data = core.get_us_year_data(year)
+    
+    return render_template('us.html',
+        autocomplete=AUTOCOMPLETE_OPTIONS,
+        data_options=DATA_OPTIONS,
+        year_options=AVAILABLE_YEARS,
+
+        selected_year=year,
+        selected_graph=graph_title,
+
+        plot_png=plot_base64, 
+        table_data=table_data,)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -109,7 +152,7 @@ def page_not_found(e):
     return render_template('error.html', errorNumber=404, errorText="""
     Uh oh! The page you are looking for does not exist. Try going back to the homepage. 
     <br>""" + str(e),
-    autocomplete=AUTOCOMPLETE_OPTIONS, data_options=DATA_OPTIONS), 404
+    autocomplete=AUTOCOMPLETE_OPTIONS), 404
 
 @app.errorhandler(500)
 def server_error(e):
@@ -117,7 +160,7 @@ def server_error(e):
     return render_template('error.html', errorNumber=500, errorText="""
     Uh oh! There was an internal server error. Sorry for the inconveniance.
     Try visiting our homepage.<br>""" + str(e),
-    autocomplete=AUTOCOMPLETE_OPTIONS, data_options=DATA_OPTIONS), 500
+    autocomplete=AUTOCOMPLETE_OPTIONS), 500
 
 if __name__ == '__main__':
     app.register_blueprint(api, url_prefix='/api')
